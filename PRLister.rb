239bed6023f3259744d6581d7fe1d6580f73266f
David Weinachter <dweinachter@fieldwire.com>
2024-04-12 09:29:03 +0200

    adding a progress bar and some colors

--- PRLister.rb
+++ PRLister.rb
@@ -1,5 +1,7 @@
 require 'octokit'
 require 'yaml'
+require 'ruby-progressbar'
+require 'colorize'
 
 # Load configuration
 config = YAML.load_file('config.yml')
@@ -13,14 +15,17 @@ def fetch_open_prs(client, repo, developers)
 end
 
 all_prs = []
+progressbar = ProgressBar.create(title: "Fetching PRs", total: config['repos'].size, format: '%a %B %p%% %t')
+
 config['repos'].each do |repo|
   prs = fetch_open_prs(client, repo, config['developers'])
   all_prs.concat(prs)
+  progressbar.increment
 end
 
 # Output the PR information
 all_prs.each do |pr|
-  puts "PR: #{pr[:title]} by #{pr[:user][:login]}"
-  puts "URL: #{pr[:html_url]}"
+  puts "PR: #{pr[:title]} by #{pr[:user][:login]}".colorize(:light_blue)
+  puts "URL: #{pr[:html_url]}".colorize(:yellow)
   puts "-" * 20
 end
